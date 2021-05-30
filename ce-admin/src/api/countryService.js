e85776c9dee5f441ed527b721de969119144a55f
dclz-tablet <duong.ht0831@gmail.com>
2021-05-30 20:02:29 +0000

    rm all

--- ce-admin/src/api/countryService.js
+++
@@ -1,8 +0,0 @@
-import http from '../core/services/http'
-import { buildQueryString } from '../util/url'
-export default {
-  getAllCountry() {
-    const url = '/countries.json'
-    return http.get(url)
-  },
-}
--- ce-admin/src/routes/helper.js
+++
@@ -1,6 +0,0 @@
-import Browser from '@/core/helpers/browser'
-import router from '@/routes/router'
-
-export const gotoSignIn = (params = {}) => {
-    router.push({ name: 'login', params: params })
-}
